0c630a1f2cdbffccf7e51f8b983d7d34dd4be1b6
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-09-18 14:30:45 -0300

    quimicos quase prontos

---
+++ Projeto Sirius/Assets/UI/QuimicoUI.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuimicoUI : MonoBehaviour
+{
+    [SerializeField] private Slider slide;
+    [SerializeField] private QuimicoPlayer qPlayer;
+
+    private void LateUpdate()
+    {
+        slide.value = qPlayer.GetQuimicoAtual();
+    }
+}
