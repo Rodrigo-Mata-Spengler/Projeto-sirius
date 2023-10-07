e475fe4e76a76dd4d3cf8fb59e2873061f1b9b5d
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-10-07 13:33:55 -0300

    Arrumando coisas de movimentação

--- Projeto Sirius/Assets/Player/Scripsts/MovimentoPlayer.cs
+++ Projeto Sirius/Assets/Player/Scripsts/MovimentoPlayer.cs
@@ -7,9 +7,17 @@ public class MovimentoPlayer : MonoBehaviour
 {
     private Rigidbody player;
     private float velocidadeAtual;
+    private Vector2 playerInput;
 
     [Header("Velocidade Caminhada")]
-    [SerializeField] private float velocidadeNormal;
+    [SerializeField] private float velocidadeNormal = 0;
+
+    [Header("velocidade Corrida")]
+    private bool correndo = false;
+    [SerializeField] private float velocidadeCorridaNormal = 0;
+
+    [Header("Pulo")]
+    [SerializeField] private float puloNormal = 0;
 
     private void Start()
     {
@@ -17,27 +25,40 @@ public class MovimentoPlayer : MonoBehaviour
     }
     public void OnMove(InputAction.CallbackContext context)
     {
-        Debug.Log("Mover:"+context.ReadValue<Vector2>());
-        velocidadeAtual = context.ReadValue<Vector2>().x;
+       Debug.Log("Mover:"+context.ReadValue<Vector2>());
+       playerInput = context.ReadValue<Vector2>();
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        Debug.Log("Jump");
+        //Debug.Log("Jump");
     }
 
     public void OnEscalar(InputAction.CallbackContext context)
     {
-        Debug.Log("Escalar:"+context.ReadValue<float>());
+        //Debug.Log("Escalar:"+context.ReadValue<float>());
     }
 
     public void OnAgacahr(InputAction.CallbackContext context)
     {
-        Debug.Log("agachar");
+        //Debug.Log("agachar");
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        correndo = !correndo;
+        
+    }
+
+    private void VelocidadeBrain()
+    {
+        
     }
 
     private void Update()
     {
-        player.velocity = new Vector3(velocidadeNormal*velocidadeAtual,0,0);
+        Debug.Log(correndo);
+        player.velocity = new Vector3(playerInput.x*velocidadeNormal,0,0);
+        //Debug.Log("velocidade:" + player.velocity);
     }
 }
