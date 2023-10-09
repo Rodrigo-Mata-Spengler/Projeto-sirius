ca547e2f511b5c33a4defc615cacb7ce89319052
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-10-09 15:24:35 -0300

    Input novo 3.0

--- Projeto Sirius/Assets/Player/Scripsts/MovimentoPlayer.cs
+++ Projeto Sirius/Assets/Player/Scripsts/MovimentoPlayer.cs
@@ -8,39 +8,66 @@ public class MovimentoPlayer : MonoBehaviour
     private Rigidbody player;
     private CharacterController playerControler;
     private float velocidadeAtual;
-    private Vector2 playerInput;
+    private Vector3 playerInput;
 
     [Header("Velocidade Caminhada")]
     [SerializeField] private float velocidadeNormal = 0;
+
+    private bool escalando = false;    
 
     [Header("velocidade Corrida")]
     private bool correndo = false;
     [SerializeField] private float velocidadeCorridaNormal = 0;
 
+    [Header("Escalada")]
+    private bool velocidade = false;
+    [SerializeField] private float velocidadeEscaladaNormal = 0;
+
     [Header("Pulo")]
-    
+    private bool pulando = false;
+    [SerializeField] private float gravidade = 0;
     [SerializeField] private float puloNormal = 0;
 
     public int a = 0;
+    private Vector3 moveDirection;
 
     private void Start()
     {
         player = GetComponent<Rigidbody>();
         playerControler = GetComponent<CharacterController>();
+        playerInput = new Vector3(0, 0, 0);
     }
+
     public void OnMove(InputAction.CallbackContext context)
     {
-       playerInput = context.ReadValue<Vector2>();
+        Vector2 temp = context.ReadValue<Vector2>();
+        playerInput.x = temp.x;
+        //playerInput.y = temp.y;
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
-        //Debug.Log("Jump");
+        if (context.ReadValue<float>() == 0)
+        {
+            pulando = false;
+        }
+        else
+        {
+            pulando = true;
+        }
+
     }
 
     public void OnEscalar(InputAction.CallbackContext context)
     {
-        //Debug.Log("Escalar:"+context.ReadValue<float>());
+        if (context.ReadValue<float>() == 0)
+        {
+            pulando = false;
+        }
+        else if()
+        {
+            pulando = true;
+        }
     }
 
     public void OnAgacahr(InputAction.CallbackContext context)
@@ -61,21 +88,63 @@ public class MovimentoPlayer : MonoBehaviour
 
     private float VelocidadeBrain()
     {
-        if (correndo)
+        float temp = 0;
+        if (escalando)
         {
-            return velocidadeCorridaNormal;
+            temp = velocidadeEscaladaNormal;
+        }else if (correndo)
+        {
+            temp = velocidadeCorridaNormal;
         }else
         {
-            return velocidadeNormal;
+            temp = velocidadeNormal;
         }
 
-        return 0;
+        return temp;
+    }
+
+    private float VelocidadeVerticalBrain()
+    {
+        float temp = 0;
+        if (playerControler.isGrounded)
+        {
+            temp = playerInput.y * puloNormal;
+        }
+        
+        //temp -= gravidade;
+
+        Debug.Log("velocidade vertical:" + temp);
+
+        return temp;
     }
 
     private void Update()
     {
+        MovimentPlayer();
+    }
+
+    private void MovimentPlayer()
+    {
         
-        player.velocity = new Vector3(playerInput.x* VelocidadeBrain(),0,0);
-        //Debug.Log("velocidade:" + player.velocity);
+        if (playerControler.isGrounded)
+        {
+            moveDirection = new Vector3(playerInput.x, 0, 0);
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= VelocidadeBrain();
+            if (pulando)
+            {
+                moveDirection.y = puloNormal;
+            }
+        }
+        else
+        {
+            moveDirection.x = playerInput.x;
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection.x *= VelocidadeBrain();
+        }
+
+        moveDirection.y -= gravidade * Time.deltaTime;
+        playerControler.Move(moveDirection * Time.deltaTime);
+
     }
 }
