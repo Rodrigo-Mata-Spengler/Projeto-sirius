2a8364a63471a7f974666d2bf5bb0961454db3d1
Rodrigo Mata <rodrigomataspengler@gmail.com>
2023-10-10 01:09:32 -0300

    quimico agora usa novo sistema de Input

--- Projeto Sirius/Assets/Player/Scripsts/QuimicoPlayer.cs
+++ Projeto Sirius/Assets/Player/Scripsts/QuimicoPlayer.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 enum Statusquimico {Abstinencia, Normal, Overdose};
 public class QuimicoPlayer : MonoBehaviour
@@ -25,6 +26,9 @@ public class QuimicoPlayer : MonoBehaviour
     [Header("Quimicos guardados")]
     [SerializeField] private int quimicos = 0;
     [SerializeField] private float quantidadeReabastecer = 0;
+    private bool aplicar = false;
+    private float tempUso = 0;
+    [SerializeField] private float delayUso = 0;
 
     [Header("Modo teste")]
     [SerializeField] private bool desligarCadencia = false;
@@ -56,15 +60,30 @@ public class QuimicoPlayer : MonoBehaviour
             status = Statusquimico.Normal;
         }
 
-        if (Input.GetButtonDown("Reabastecer"))
+        if (aplicar && tempUso <= Time.time)
         {
-            if(quimicos > 0)
+            Debug.Log("usou");
+            if (quimicos > 0)
             {
                 quimicoatual += quantidadeReabastecer;
                 quimicos -= 1;
+                tempUso = Time.time + delayUso;
             }
         }
     }
+
+    public void OnAplicar(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 0)
+        {
+            aplicar = false;
+        }
+        else
+        {
+            aplicar = true;
+        }
+    }
+
     public void ReabastecerQuimico(int a)
     {
         quimicos += a;
