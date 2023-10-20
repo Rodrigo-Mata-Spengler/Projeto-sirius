e0d91033ecd781091f42fe9d00a015fb1db76485
Rodrigo-Mata-Spengler <61998817+Rodrigo-Mata-Spengler@users.noreply.github.com>
2023-10-20 09:02:55 -0300

    arrumando detalhes do sytema de save

--- Projeto Sirius/Assets/Save system/Player Spawn/PlayerSpawn.cs
+++ Projeto Sirius/Assets/Save system/Player Spawn/PlayerSpawn.cs
@@ -9,26 +9,26 @@ public class PlayerSpawn : MonoBehaviour
         //seta do gizmo
         Gizmos.color = Color.green;
         //topo
-        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y + 2, transform.position.z), new Vector3(transform.position.x - 0.5f, transform.position.y + 2, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z));
         //barra lateral
-        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y + 2, transform.position.z), new Vector3(transform.position.x + 0.5f, transform.position.y+1, transform.position.z));
-        Gizmos.DrawLine(new Vector3(transform.position.x - 0.5f, transform.position.y + 2, transform.position.z), new Vector3(transform.position.x - 0.5f, transform.position.y+1, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z));
         //base cabeça
-        Gizmos.DrawLine(new Vector3(transform.position.x - 0.5f, transform.position.y+1, transform.position.z), new Vector3(transform.position.x - 0.75f, transform.position.y+1, transform.position.z));
-        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y+1, transform.position.z), new Vector3(transform.position.x + 0.75f, transform.position.y+1, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x - 0.5f, transform.position.y, transform.position.z), new Vector3(transform.position.x - 0.75f, transform.position.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.5f, transform.position.y, transform.position.z), new Vector3(transform.position.x + 0.75f, transform.position.y, transform.position.z));
         //seta
-        Gizmos.DrawLine(new Vector3(transform.position.x - 0.75f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x, transform.position.y, transform.position.z));
-        Gizmos.DrawLine(new Vector3(transform.position.x + 0.75f, transform.position.y + 1, transform.position.z), new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x - 0.75f, transform.position.y, transform.position.z), new Vector3(transform.position.x, transform.position.y-1, transform.position.z));
+        Gizmos.DrawLine(new Vector3(transform.position.x + 0.75f, transform.position.y, transform.position.z), new Vector3(transform.position.x, transform.position.y-1, transform.position.z));
     }
     //spawn simples
     public bool SpawnPlayer(GameObject player)
     {
-        return Instantiate(player,new Vector3(transform.position.x, transform.position.y+1, transform.position.z), transform.rotation);
+        return Instantiate(player,new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
     }
     //spawn com Save
     public bool SpawnPlayer(GameObject player, PlayerData data)
     {
-        GameObject local_PLayer = Instantiate(player, new Vector3(transform.position.x, transform.position.y + 1, transform.position.z), transform.rotation);
+        GameObject local_PLayer = Instantiate(player, new Vector3(transform.position.x, transform.position.y, transform.position.z), transform.rotation);
 
         local_PLayer.GetComponent<QuimicoPlayer>().SetQuimicoAtual(data.quantidade_Aplicada);
         local_PLayer.GetComponent<QuimicoPlayer>().SetQuimico(data.quantidade_Guardada);
