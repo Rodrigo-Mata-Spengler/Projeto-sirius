1e31cd4a62c55bae88830cef1377aa2000e33950
Rodrigo-Mata-Spengler <61998817+Rodrigo-Mata-Spengler@users.noreply.github.com>
2023-11-06 17:00:21 -0300

    temporizador de porta

--- Projeto Sirius/Assets/Objetos/scripts/DoorButton.cs
+++ Projeto Sirius/Assets/Objetos/scripts/DoorButton.cs
@@ -7,15 +7,45 @@ public class DoorButton : MonoBehaviour
 
     [SerializeField] private Door[] Door;
 
-    public void OpenDoor(Transform Player)
-    {
-        for (int i = 0; i < Door.Length; i++)
-        {
-            if (!Door[i].IsOpen)
-            {
-                Door[i].Open(Player.transform.position);
+    [SerializeField] private bool timer = false;
+    [SerializeField] private float tempoLigado = 0;
+    private float tempoFechar = 0;
 
+    private void Update()
+    {
+        if (timer)
+        {
+            if (tempoFechar <= Time.time)
+            {
+                for (int i = 0; i < Door.Length; i++)
+                {
+                    if (!Door[i].IsOpen)
+                    {
+                        Door[i].Close();
+
+                    }
+                }
             }
         }
+    }
+
+    public void OpenDoor(Transform Player)
+    {
+        if (!timer)
+        {
+            for (int i = 0; i < Door.Length; i++)
+            {
+                if (!Door[i].IsOpen)
+                {
+                    Door[i].Open(Player.transform.position);
+
+                }
+            }
+        }else
+        {
+            tempoFechar = tempoLigado + Time.time;
+        }
+
+        
     }
 }
