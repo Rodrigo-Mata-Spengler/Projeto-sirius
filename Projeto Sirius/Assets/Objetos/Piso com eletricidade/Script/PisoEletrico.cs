e7195a7f2877f02c8493000a31c86e28d16fdec9
Rodrigo-Mata-Spengler <61998817+Rodrigo-Mata-Spengler@users.noreply.github.com>
2023-11-06 16:53:57 -0300

    chão eletrico

---
+++ Projeto Sirius/Assets/Objetos/Piso com eletricidade/Script/PisoEletrico.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PisoEletrico : MonoBehaviour
+{
+    [Header("Menu Pause")]
+    [SerializeField] private PauseMenu menuMorte;
+
+    [Header("Tag Player")]
+    [SerializeField] private string playerTag;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            menuMorte.Death();
+        }
+    }
+}
