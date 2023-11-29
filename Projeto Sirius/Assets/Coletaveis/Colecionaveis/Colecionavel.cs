bbfb1efdeef2c363f80388f27c9cc3e6e515c452
Rodrigo-Mata-Spengler <61998817+Rodrigo-Mata-Spengler@users.noreply.github.com>
2023-11-29 11:06:12 -0300

    colecionaveis arte

--- Projeto Sirius/Assets/Coletaveis/Colecionaveis/Colecionavel.cs
+++ Projeto Sirius/Assets/Coletaveis/Colecionaveis/Colecionavel.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Colecionavel : MonoBehaviour
 {
     public int colecionavelID;
+
+    [SerializeField] private TextMeshPro texto;
+    [SerializeField] private string numero = "num";
 
     private void Awake()
     {
@@ -12,8 +16,10 @@ public class Colecionavel : MonoBehaviour
 
         if (data.colecionaveis[colecionavelID])
         {
-            Destroy(this);
+            Destroy(this.gameObject);
         }
         data = null;
+
+        texto.text = numero;
     }
 }
